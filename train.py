07185799ea3382d2bdd4221e7b83c107d897eb34
CMazuecos <202106145@alu.comillas.edu>
2025-09-26 08:27:22 +0200

    update dataset and train.py

--- train.py
+++ train.py
@@ -6,10 +6,33 @@ from sklearn.metrics import accuracy_score
 import joblib
 import mlflow
 import mlflow.sklearn
-# Cargar el conjunto de datos
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+import dagshub
+import os
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+import joblib
+import mlflow
+import mlflow.sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+dagshub.init(repo_owner='CMazuecos', repo_name='mlops-practica-icai', mlflow=True)
+
+
+# Cargar el conjunto de datos desde el archivo CSV
+try:
+    iris = pd.read_csv('data/iris_dataset.csv')
+except FileNotFoundError:
+    print("Error: El archivo 'data/iris_dataset.csv' no fue encontrado.")
+
+# Dividir el DataFrame en características (X) y etiquetas (y)
+X = iris.drop('target', axis=1)
+y = iris['target']
+
 # Iniciar un experimento de MLflow
 with mlflow.start_run():
     # Dividir los datos en conjuntos de entrenamiento y prueba
@@ -17,7 +40,7 @@ with mlflow.start_run():
     X, y, test_size=0.3, random_state=42
     )
     # Inicializar y entrenar el modelo
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier(n_estimators=300, random_state=42)
     model.fit(X_train, y_train)
     # Realizar predicciones y calcular la precisión
     y_pred = model.predict(X_test)
@@ -27,7 +50,7 @@ with mlflow.start_run():
     # Registrar el modelo con MLflow
     mlflow.sklearn.log_model(model, "random-forest-model")
     # Registrar parámetros y métricas
-    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("n_estimators", 300)
     mlflow.log_metric("accuracy", accuracy)
     print(f"Modelo entrenado y precisión: {accuracy:.4f}")
     print("Experimento registrado con MLflow.")
